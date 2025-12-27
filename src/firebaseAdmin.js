9b0b9eaf5d0fc9d02e4ac1b7614b39290f92c629
Benjie-san <dev.benjamin.perez@gmail.com>
2025-12-28 00:54:36 +0800

    few changes in firebase admin

--- src/firebaseAdmin.js
+++ src/firebaseAdmin.js
@@ -1,6 +1,29 @@
 // src/firebaseAdmin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-service-account.json");
+
+function loadServiceAccount() {
+    if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+        try {
+            return JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+        } catch (err) {
+            throw new Error("Invalid FIREBASE_SERVICE_ACCOUNT JSON");
+        }
+    }
+    if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+        try {
+            const json = Buffer.from(
+                process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
+                "base64"
+            ).toString("utf8");
+            return JSON.parse(json);
+        } catch (err) {
+            throw new Error("Invalid FIREBASE_SERVICE_ACCOUNT_BASE64");
+        }
+    }
+    return require("../firebase-service-account.json");
+}
+
+const serviceAccount = loadServiceAccount();
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
